b936f4d70ca692ccacdb141216020892cb24ae59
Andrea zucchi <azcine@gmail.com>
2021-11-12 23:05:18 -0300

    Cards con jquery

--- scripts/control_dom.js
+++ scripts/control_dom.js
@@ -1,23 +1,31 @@
-const pizzas = [
-    { id: 1, name: 'Muzarela', price: 800, description: 'Muzarela', img: 'https://www.placecage.com/c/200/200' },
-    { id: 2, name: 'Muzarela Vegana', price: 800, description: 'muzarelaVegana', img: 'https://www.placecage.com/c/200/200' },
-    { id: 3, name: 'Verduras', price: 1000, description: 'verduras', img: 'https://www.placecage.com/c/200/200' },
-    { id: 4, name: 'Napolitana', price: 900, description: 'napolitana', img: 'https://www.placecage.com/c/200/200' }
-]
+$(document).ready(function() {
+    const pizzas = [
+        { id: 1, name: 'Muzarela', price: 800, description: 'Muzarela', img: 'https://www.placecage.com/c/200/200' },
+        { id: 2, name: 'Muzarela Vegana', price: 800, description: 'muzarelaVegana', img: 'https://www.placecage.com/c/200/200' },
+        { id: 3, name: 'Verduras', price: 1000, description: 'verduras', img: 'https://www.placecage.com/c/200/200' },
+        { id: 4, name: 'Napolitana', price: 900, description: 'Napolitana', img: 'https://www.placecage.com/c/200/200' },
+        { id: 5, name: 'Margarita ', price: 900, description: 'Margarita', img: 'https://www.placecage.com/c/200/200' }
+    ]
 
-console.log(pizzas)
-
-// Obtengo Nodo productos__container
-document.getElementById("productos__container").innerHTML = pizzas.map(pizza => {
-    return `
-        <article class="card">
-            <img src=${pizza.img} alt="${pizza.description}">
-            <div class="container__card">
-                <h4 class="text-center">${pizza.name}</h4>
-                <p>${pizza.description}</p>
-                <p>Precio: ${pizza.price}</p>
-                <input type="submit" value="COMPRAR">
+    console.log(pizzas)
+    var cards = '';
+    // Obtengo Nodo productos__container
+    pizzas.map(pizza => {
+        cards = `
+        <div class="col">
+            <div class="card text-center">
+                <img src=${pizza.img} class="card-img-top" alt="${pizza.description}">
+                <div class="card-body">
+                    <h5 class="card-title">${pizza.name}</h5>
+                    <p class="card-text">${pizza.description}</p>
+                    <p class="card-text">Precio: $${pizza.price}</p>
+                    <button type="button" class="btn btn-secondary" id="agregar__carrito__${pizza.id}">Agregar al carrito</button>
+                </div>
             </div>
-        </article>
-    `
+        </div>
+    ` + cards;
+    })
+
+    $("#productos__container").append(cards)
+
 })
