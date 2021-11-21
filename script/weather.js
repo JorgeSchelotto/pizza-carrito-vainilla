652fdf8b9d20c3bdb78c30f661f67df4eb7a2654
Andrea Zucchi <azcine@gmail.com>
2021-11-21 19:02:45 -0300

    App Clima

---
+++ script/weather.js
@@ -0,0 +1,49 @@
+$(document).ready(function() {
+
+    let contenido = `<p>Pais: <span id="pais"></span></p>
+    <p> Ciudad: <span id="ciudad"></span></p>
+    <p>Temperatura: </p>
+    <p><img id="icono" src=""></img>
+    </p>
+    <p><span id="temperatura"></span></p>
+    <p>Humedad: <span id="humedad"></span></p>
+    <p> Viento: <span id="viento"></span></p>
+    <p>Descripcion: <span id="descripcion"></span></p> `;
+
+    let addContenido = function(contenidoHtml) {
+        $("#clima").html(contenidoHtml);
+    }
+
+    $(document).on("click", "#input__ciudad", (event) => {
+        event.preventDefault();
+        let ciudad = $("#select__ciudad").val();
+        $.ajax({
+            url: "https://api.openweathermap.org/data/2.5/weather",
+            type: "GET",
+            data: {
+                q: `${ciudad},ar`,
+                appid: "dbbd01c29ac808ba210959454a7881a1",
+                dataType: "jsonp",
+                units: "metric",
+                lang: "es",
+            },
+            success: function(data) {
+                addContenido(contenido);
+                console.log(data);
+                $("#temperatura").html(data.main.temp);
+                $("#humedad").html(data.main.humidity);
+                $("#viento").html(data.wind.speed);
+                $("#ciudad").html(data.name);
+                $("#pais").html(data.sys.country);
+                $("#descripcion").html(data.weather[0].description);
+                $("#icono").attr("src", "http://openweathermap.org/img/w/" + data.weather[0].icon + ".png");
+
+            },
+            error: function(error) {
+                console.log(error);
+            }
+        });
+
+
+    });
+});
