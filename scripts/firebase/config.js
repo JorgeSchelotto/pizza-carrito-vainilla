b1a021cbb2e2c6ed22f21a691b1bc4e028dc6f3c
Jorge Schelotto <jasfotografo@hotmail.com>
2021-11-22 16:12:09 -0300

    Fix Config gitignore

---
+++ scripts/firebase/config.js
@@ -0,0 +1,15 @@
+// Firebase Config
+
+const firebaseConfig = {
+    apiKey: "AIzaSyCLLkL0c_DROuaZJVNUbnpvGTYq5CKxOtQ",
+    authDomain: "pizzeriajavascript.firebaseapp.com",
+    databaseURL: "https://pizzeriajavascript-default-rtdb.firebaseio.com",
+    projectId: "pizzeriajavascript",
+    storageBucket: "pizzeriajavascript.appspot.com",
+    messagingSenderId: "964665440891",
+    appId: "1:964665440891:web:012d10ad3f4d7e15d880de"
+};
+
+
+// Exports
+export default firebaseConfig;
